77962885121a6c4eeef0e107def3fae4e1752a02
jorisreynes <joris.reynes81@gmail.com>
2023-10-20 22:07:10 +0200

    add cors

--- src/main/java/com/example/demo/controller/GamesController.java
+++ src/main/java/com/example/demo/controller/GamesController.java
@@ -3,13 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.model.Game;
 import com.example.demo.service.GamesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins="*")
 @RestController
 public class GamesController {
 
